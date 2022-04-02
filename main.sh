234327248e7c2c71b4fb654e1de595644ddeb492
TECH CODE <86469918+techcode1001@users.noreply.github.com>
2022-04-02 08:44:08 +0530

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+VBR="1000k"
+FPS="30"
+QUAL="ultrafast"
+
+YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2"
+KEY="urff-w6zh-amub-g08f-4cxj"
+
+VIDEO_SOURCE="video.mp4"
+AUDIO_SOURCE="http://stream.zeno.fm/kq34w27yss8uv"
+
+ffmpeg \
+-re -f lavfi -i "movie=filename=$VIDEO_SOURCE:loop=0, setpts=N/(FRAME_RATE*TB)" \
+-thread_queue_size 512 -i "$AUDIO_SOURCE" \
+-map 0:v:0 -map 1:a:0 \
+-map_metadata:g 1:g \
+-vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
+-acodec libmp3lame -ar 44100 -threads 6 -qscale:v 3 -b:a 320000 -bufsize 512k \
+-f flv "$YOUTUBE_URL/$KEY"
